89ec463be06715be3da759db607ed9ee1012b5a9
riorocker75 <gamesourmakatab@gmail.com>
2024-08-22 13:48:14 +0700

    doi0

--- resources/views/layouts/front_app.blade.php
+++ resources/views/layouts/front_app.blade.php
@@ -52,7 +52,90 @@
 
 
 
-
+<!-- footer start -->
+<footer id="gen-footer">
+    <div class="gen-footer-style-1">
+        <div class="gen-footer-top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-3 col-md-6">
+                        <div class="widget">
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <h4 class="footer-title">Downlaod App</h4>
+                                    <p>Platform streaming film yang menarik hadir di indonesia.
+                                    </p>
+                                    <ul class="social-link">
+                                        <li><a href="#" class="facebook"><i class="fab fa-facebook-f"></i></a></li>
+                                        <li><a href="#" class="facebook"><i class="fab fa-instagram"></i></a></li>
+                                        <li><a href="#" class="facebook"><i class="fab fa-skype"></i></a></li>
+                                        <li><a href="#" class="facebook"><i class="fab fa-twitter"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-md-6">
+                        <div class="widget">
+                            <h4 class="footer-title">Explore</h4>
+                            <div class="menu-explore-container">
+                                <ul class="menu">
+                                    <li class="menu-item">
+                                        <a href="{{url('/')}}" aria-current="page">Home</a>
+                                    </li>
+                                    <li class="menu-item"><a href="{{url('/news/all')}}">News</a></li>
+                                    <li class="menu-item"><a href="{{url('/movies/all')}}">Movies</a></li>
+                              
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-md-6">
+                        <div class="widget">
+                            {{-- <h4 class="footer-title">Company</h4>
+                            <div class="menu-about-container">
+                                <ul class="menu">
+                                    <li class="menu-item"><a href="#">Company</a>
+                                    </li>
+                                    <li class="menu-item"><a href="#">Privacy
+                                            Policy</a></li>
+                                    <li class="menu-item"><a href="#">Terms Of
+                                            Use</a></li>
+                                    <li class="menu-item"><a href="#">Help
+                                            Center</a></li>
+     
+                                </ul>
+                            </div> --}}
+                        </div>
+                    </div>
+                    <div class="col-xl-3  col-md-6">
+                        <div class="widget">
+                            <h4 class="footer-title">Kebijakan & privasi</h4>
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <p>Semua konten ini hanya sebuah penelitian dan dipergunakan sebagaimana mestinya
+                                    </p>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="gen-copyright-footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 align-self-center">
+                        <span class="gen-copyright"><a target="_blank" href="#"> Copyright {{date('Y')}} SafiraSalsabila All Rights
+                                Reserved.</a></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- footer End -->
 
 
 
